7df62fbf19f1daaa9336e877160c68f698c5774c
peterndungukamau <peterndungukamau9@gmail.com>
2019-11-05 04:14:49 +0300

    import service to app module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ProfileService } from './services/profile.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProfileComponent } from './profile/profile.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProfileService ],
+ 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
