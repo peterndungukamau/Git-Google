72aee9d1952851b4844e67cbe5f4286445daa675
peterndungukamau <peterndungukamau9@gmail.com>
2019-11-05 04:46:30 +0300

    add bootstrap

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,5 +1,4 @@
  import { Injectable } from '@angular/core';
- import { Http, Headers } from '@angular/http';
  import { HttpClient } from '@angular/common/http';
  import 'rxjs/add/operator/map';
 
@@ -9,10 +8,10 @@
 export class ProfileService {
 
   private username:string;
-  private clientid = '';
-  private clientsecret = '';
+  private clientid = '347bc0575ae9735b5768';
+  private clientsecret = '81afaaa481cd770a2b991c68df83cd0cad61fd8b';
 
-  constructor(private http:Http) {
+  constructor(private http:HttpClient) {
     console.log("service is ready");
     this.username = 'peterndungukamau';
    }
