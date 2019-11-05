54c1cd14182d94989fcb10b4cdc987ed19861eb8
peterndungukamau <peterndungukamau9@gmail.com>
2019-11-05 04:10:28 +0300

    modify service.ts

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,5 +1,6 @@
  import { Injectable } from '@angular/core';
  import { Http, Headers } from '@angular/http';
+ import { HttpClient } from '@angular/common/http';
  import 'rxjs/add/operator/map';
 
 @Injectable({
