7219b8707a9b31831ff03d2b9de247daa7b124e9
peterndungukamau <peterndungukamau9@gmail.com>
2019-11-05 04:08:20 +0300

    add both the client id and secret to service

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+ import { Injectable } from '@angular/core';
+ import { Http, Headers } from '@angular/http';
+ import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username:string;
+  private clientid = '';
+  private clientsecret = '';
+
+  constructor(private http:Http) {
+    console.log("service is ready");
+    this.username = 'peterndungukamau';
+   }
+
+   getProfileInfo(){
+     return this.http.get("https://api.github/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+     .map(res => res.json());
+   }
 }
